d9225b9721eb49f5b6da2588eb06213a549e4294
Hiromasa Ihara <iharahiromasa@gmail.com>
2024-03-14 11:40:02 +0900

    feat: add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bump-ansible-requirements
+
+
+    $ git clone ...
+    $ cd bump-ansible-requirements/
+    $ pip install -r requirements.txt
+    $ python bump-ansible-requirements.py # in default, try to use requirements.yml
+    $ python bump-ansible-requirements.py --requirements-file requirements.yaml
---
+++ bump-ansible-requirements.py
@@ -0,0 +1,30 @@
+import click
+
+import requests
+
+from ruamel.yaml import YAML
+
+
+@click.command()
+@click.option('--requirements-file', default='requirements.yml')
+def main(requirements_file):
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    yaml.indent(mapping=2, sequence=4, offset=2)
+
+    with open(requirements_file, 'r') as file:
+        data = yaml.load(file)
+
+    for (i, collection) in enumerate(data['collections']):
+        namespace, name = collection['name'].split('.')
+        url = f'https://galaxy.ansible.com/api/v3/plugin/ansible/content/published/collections/index/{ namespace }/{ name}/'
+        r = requests.get(url)
+        highest_version = r.json()['highest_version']['version']
+        data['collections'][i]['version'] = highest_version
+
+    with open('requirements.yml', 'w') as file:
+        yaml.dump(data, file)
+
+
+if __name__ == '__main__':
+    main()
